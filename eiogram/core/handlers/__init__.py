a23f0ec1980f6f4c6b5146a6ff1e6cd41be378b9
erfjab <erfjab@gmail.com>
2025-05-20 01:29:46 +0330

    ref: build library

--- eiogram/core/handlers/__init__.py
+++ eiogram/core/handlers/__init__.py
@@ -1,7 +1,6 @@
 from ._error import ErrorHandler
 from ._callback import CallbackHandler
 from ._message import MessageHandler
-from ._middlewares import MiddlewareHandler, Middleware
 from ._base import Handler, BaseHandler, FilterFunc, HandlerFunc
 
 __all__ = [
