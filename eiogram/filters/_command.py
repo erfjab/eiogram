0dfc4302c7f3886afb55c1e0e6ce5259c37b96e5
Erfan <143827987+erfjab@users.noreply.github.com>
2025-05-26 01:52:24 +0330

    improve: add args count to command filter (#26)

--- eiogram/filters/_command.py
+++ eiogram/filters/_command.py
@@ -3,11 +3,24 @@ from ._base import _BaseTextFilter
 
 
 class Command(_BaseTextFilter):
-    """Filter commands in text/caption"""
+    """Simple command filter with exact argument count"""
 
-    def __init__(self, command: str = "start", context: bool = False):
-        cmd = command.lower().strip("/")
-        super().__init__(lambda t: t.lower().startswith(f"/{cmd}"), context)
+    def __init__(
+        self,
+        command: str,
+        *,
+        args_count: int = 0,
+        prefix: str = "/",
+        context: bool = False,
+    ):
+        cmd = command.lower().strip(prefix)
+
+        def check_func(text: str) -> bool:
+            text = text.lower().strip()
+            parts = text.split()
+            return len(parts) == args_count + 1 and parts[0] == f"{prefix}{cmd}"
+
+        super().__init__(check_func, context)
 
 
 StartCommand = partial(Command, command="start")
