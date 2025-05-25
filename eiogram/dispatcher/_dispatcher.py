1ab605adaf90b3054cb14b4fb4bcd6fe8e010275
erfjab <erfjab@gmail.com>
2025-05-26 01:12:53 +0330

    feat: add outer middlewares

--- eiogram/dispatcher/_dispatcher.py
+++ eiogram/dispatcher/_dispatcher.py
@@ -7,6 +7,7 @@ from ..types import Update, Message, CallbackQuery
 from ..stats.storage import BaseStorage, MemoryStorage
 from ..stats import StatsManager
 from ..utils.callback_data import CallbackData
+from ..middleware import BaseMiddleware
 
 U = TypeVar("U", bound=Union[Update, Message, CallbackQuery])
 
@@ -15,12 +16,29 @@ class Dispatcher:
     def __init__(self, bot: Bot, storage: Optional[BaseStorage] = None):
         self.bot = bot
         self.routers: List[Router] = []
-        self.storage = storage or MemoryStorage()
+        self.storage: BaseStorage = storage or MemoryStorage()
+        self.outer_middlewares: List[BaseMiddleware] = []
 
     def include_router(self, router: "Router") -> None:
         self.routers.append(router)
 
+    def outer_middleware(self, middleware: Callable) -> Callable:
+        self.outer_middlewares.append(middleware)
+        return middleware
+
     async def process(self, update: Update) -> None:
+        wrapped_process = self._wrap_outer_middlewares(self._inner_process)
+        await wrapped_process(update)
+
+    def _wrap_outer_middlewares(self, handler: Callable) -> Callable:
+        wrapped_handler = handler
+        for middleware in reversed(self.outer_middlewares):
+            wrapped_handler = self._create_middleware_wrapper(
+                middleware, wrapped_handler
+            )
+        return wrapped_handler
+
+    async def _inner_process(self, update: Update) -> None:
         handler, middlewares = await self._find_handler(update=update)
         if not handler:
             return
@@ -29,6 +47,16 @@ class Dispatcher:
         wrapped_handler = self._wrap_middlewares(middlewares.middlewares, final_handler)
 
         await wrapped_handler(update, {})
+
+    async def _find_handler(
+        self, update: Update
+    ) -> Optional[Tuple[Handler, MiddlewareHandler]]:
+        stats = await self.storage.get_stats(update.origin.from_user.chatid)
+        for router in self.routers:
+            handler = await router.matches_update(update=update, stats=stats)
+            if handler:
+                return handler, router.middleware
+        return None, None
 
     def _wrap_middlewares(
         self, middlewares: List[Callable], final_handler: Callable
@@ -57,16 +85,6 @@ class Dispatcher:
             return await handler(**kwargs)
 
         return final_handler
-
-    async def _find_handler(
-        self, update: Update
-    ) -> Optional[Tuple[Handler, MiddlewareHandler]]:
-        stats = await self.storage.get_stats(update.origin.from_user.chatid)
-        for router in self.routers:
-            handler = await router.matches_update(update=update, stats=stats)
-            if handler:
-                return handler, router.middleware
-        return None, None
 
     async def _build_handler_kwargs(
         self, handler: Callable, update: Update, middleware_data: Dict[str, Any]
