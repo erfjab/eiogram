57a78bcf06bc861a836a20ae118cff0317179c74
erfjab <erfjab@gmail.com>
2025-05-26 00:16:24 +0330

    chore: del compiste filters

--- eiogram/filters/__init__.py
+++ eiogram/filters/__init__.py
@@ -1,7 +1,6 @@
 from ._base import Filter
 from ._chat_type import IsSuperGroup, IsChannel, IsForum, IsGroup, IsPrivate
 from ._command import Command, StartCommand, VersionCommand
-from ._composite import And, Or, Not
 from ._data import Data
 from ._photo import Photo
 from ._regax import Regex
@@ -19,9 +18,6 @@ __all__ = [
     "Command",
     "StartCommand",
     "VersionCommand",
-    "And",
-    "Or",
-    "Not",
     "Data",
     "Photo",
     "Regex",
