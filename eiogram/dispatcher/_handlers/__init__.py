54f69549e5f8d3c13448903817c63d5bfe242d4c
Erfan <143827987+erfjab@users.noreply.github.com>
2025-05-27 11:08:11 +0330

    feat: add fallback handler (#31)
    * feat: add fallback handler
    
    * ref: fallback

--- eiogram/dispatcher/_handlers/__init__.py
+++ eiogram/dispatcher/_handlers/__init__.py
@@ -3,8 +3,10 @@ from ._callback_query import CallbackQueryHandler
 from ._message import MessageHandler
 from ._base import Handler, BaseHandler, FilterFunc, HandlerFunc
 from ._middleware import MiddlewareHandler
+from ._fallback import FallbackHandler
 
 __all__ = [
+    "FallbackHandler",
     "ErrorHandler",
     "CallbackQueryHandler",
     "MessageHandler",
