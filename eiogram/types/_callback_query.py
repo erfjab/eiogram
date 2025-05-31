848e2738e2dd6b1d76de73cc70b13d90ebce0b0f
Erfan <143827987+erfjab@users.noreply.github.com>
2025-05-31 20:36:17 +0330

    chore: fix inline builder keyboard and add chat to callbackquery (#33)
    * chore: add chat to callbackquery
    
    * chore: fix inline builder keyboard

--- eiogram/types/_callback_query.py
+++ eiogram/types/_callback_query.py
@@ -2,6 +2,7 @@ from typing import Optional
 from pydantic import BaseModel, Field
 from ._user import User
 from ._message import Message
+from ._chat import Chat
 from ..client import Bot
 
 
@@ -18,6 +19,10 @@ class CallbackQuery(BaseModel):
 
     def __str__(self) -> str:
         return f"CallbackQuery(id={self.id}, from={self.from_user.full_name}, data={self.data})"
+
+    @property
+    def chat(self) -> Optional[Chat]:
+        return self.message.chat if self.message else None
 
     def answer(
         self, text: Optional[str] = None, show_alert: Optional[bool] = None
