b5036756d32f20b9a08a226d2bfba82173b21ea5
erfjab <erfjab@gmail.com>
2025-07-04 17:27:56 +0330

    improve: add **kwargs to stats manager

--- eiogram/stats/_manager.py
+++ eiogram/stats/_manager.py
@@ -8,23 +8,23 @@ class StatsManager:
         self.storage = storage
         self.key = key
 
-    async def set_state(self, state: State) -> None:
-        await self.storage.set_stats(self.key, stats=state.name)
+    async def set_state(self, state: State, **kwargs: Any) -> None:
+        await self.storage.set_stats(self.key, stats=state.name, **kwargs)
 
-    async def get_state(self) -> Optional[State]:
-        return await self.storage.get_stats(self.key)
+    async def get_state(self, **kwargs: Any) -> Optional[State]:
+        return await self.storage.get_stats(self.key, **kwargs)
 
-    async def upsert_data(self, **data: Any) -> None:
-        return await self.storage.upsert_data(self.key, **data)
+    async def upsert_data(self, **kwargs: Any) -> None:
+        return await self.storage.upsert_data(self.key, **kwargs)
 
-    async def get_data(self) -> Dict[str, Any]:
-        return await self.storage.get_data(self.key)
+    async def get_data(self, **kwargs: Any) -> Dict[str, Any]:
+        return await self.storage.get_data(self.key, **kwargs)
 
-    async def clear_all(self) -> None:
-        await self.storage.clear_all(self.key)
+    async def clear_all(self, **kwargs: Any) -> None:
+        await self.storage.clear_all(self.key, **kwargs)
 
-    async def clear_stats(self) -> None:
-        await self.storage.clear_stats(self.key)
+    async def clear_stats(self, **kwargs: Any) -> None:
+        await self.storage.clear_stats(self.key, **kwargs)
 
-    async def clear_data(self) -> None:
-        await self.storage.clear_data(self.key)
+    async def clear_data(self, **kwargs: Any) -> None:
+        await self.storage.clear_data(self.key, **kwargs)
