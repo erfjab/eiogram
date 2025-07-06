875a50484802670b136a0aa539446d08649ac6de
erfjab <erfjab@gmail.com>
2025-07-07 02:00:23 +0330

    fix: filter

--- eiogram/filters/_state.py
+++ eiogram/filters/_state.py
@@ -5,9 +5,9 @@ from ..state import State
 
 class StateFilter(Filter):
     def __init__(self, state: Optional[State]):
-        super().__init__(lambda state: (state is not None and state == state.name))
+        super().__init__(lambda s: (s is not None and s == state.name))
 
 
 class IgnoreStateFilter(Filter):
     def __init__(self):
-        super().__init__(lambda x: True)
+        super().__init__(lambda _: True)
