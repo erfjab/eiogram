5109f950aaa521180a3247e048ce2067e8f9197f
Erfan <143827987+erfjab@users.noreply.github.com>
2025-11-27 01:18:40 +0330

    feat[messages]: add EditMessageReplyMarkup and EditMessageText methods (#61)

--- eiogram/client/_bot.py
+++ eiogram/client/_bot.py
@@ -174,6 +174,34 @@ class Bot:
             reply_markup=reply_markup,
         )
 
+    async def edit_message_text(
+        self,
+        chat_id: Union[int, str],
+        message_id: int,
+        text: str,
+    ) -> "Message":
+        from .methods._edit_message_text import EditMessageText
+
+        return await EditMessageText(self.token).execute(
+            chat_id=chat_id,
+            message_id=message_id,
+            text=text,
+        )
+
+    async def edit_message_reply_markup(
+        self,
+        chat_id: Union[int, str],
+        message_id: int,
+        reply_markup: "InlineKeyboardMarkup",
+    ) -> "Message":
+        from .methods._edit_message_reply_markup import EditMessageReplyMarkup
+
+        return await EditMessageReplyMarkup(self.token).execute(
+            chat_id=chat_id,
+            message_id=message_id,
+            reply_markup=reply_markup,
+        )
+
     async def send_photo(
         self,
         chat_id: Union[int, str],
