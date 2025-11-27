baa2ae6d56f59719e63a4aaf7fc93fd01680b41d
Erfan <143827987+erfjab@users.noreply.github.com>
2025-11-27 16:31:57 +0330

    feat[filters]: add chat and contact filters (#68)

--- eiogram/filters/__init__.py
+++ eiogram/filters/__init__.py
@@ -6,6 +6,8 @@ from ._photo import Photo
 from ._regax import Regex
 from ._text import Text
 from ._state import StateFilter, IgnoreStateFilter
+from ._contact import Contact
+from ._shared import ShareUser, ShareChat
 
 __all__ = [
     "BaseTextFilter",
@@ -23,4 +25,7 @@ __all__ = [
     "Text",
     "StateFilter",
     "IgnoreStateFilter",
+    "Contact",
+    "ShareUser",
+    "ShareChat",
 ]
